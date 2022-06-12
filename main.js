0844c145b642b98d1cae68182e7097ce1f1fc252
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-12 16:37:47 +0200

    add print options function

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const newAdviceBtn = document.querySelector("button");
 const cleanBtn = document.querySelector("#clean");
-const showAdvice = document.querySelector("#showAdvice");
-const showOptionBtn = document.querySelector("#showOption");
+const showAdviceBtn = document.querySelector("#showAdvice");
+const showOptionBtn = document.querySelector("#showOptions");
 const h1 = document.querySelector('h1');
+const div = document.querySelector('div');
 
 const advices = ["Działaj!", "Przemyśl to..."];
 
@@ -20,24 +21,19 @@ const cleaning = (e) => {
 
     advices.length = 2;
     console.log(advices);
-
-
 }
 
 const adviceGenerator = () => {
     const randomNumber = Math.floor(Math.random() * advices.length);
     h1.textContent = `Moja rada to: ${advices[randomNumber]}`;
-
-    
 }
 
+const printOptions = () => {
+    alert(`wszystkie zapisane możliwości to: ${advices}`);
 
-
-
+}
 
 newAdviceBtn.addEventListener('click', addAdvice);
 cleanBtn.addEventListener('click', cleaning);
-showAdvice.addEventListener('click', adviceGenerator);
-
-
-
+showAdviceBtn.addEventListener('click', adviceGenerator);
+showOptionBtn.addEventListener('click', printOptions);
